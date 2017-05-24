b0e16b0386315a437276737ea73333a2edc8bea3
DoktorFaust <dokfaust@autistici.org>
2017-05-24 23:37:46 +0200

    First on pycazz

--- networkx/Graph.py
+++ networkx/Graph.py
@@ -1,17 +1,81 @@
-import csv
-import sys
-
 import networkx as nx
+import random as rnd
+import numpy as np
+import re
 
-G = nx.DiGraph()
+from sympy import *
+from lea   import *
 
-f = open(sys.argv[1], 'rt')
-try:
-    reader = csv.reader(f)
-    for row in reader:
-        G.add_edge(row[3], row[5])
+G = nx.Graph()
 
-    print G.edges()
-        
-finally:
-    f.close()
+filename1 = "feed-100014305273231.csv"
+
+user1 = filename1[5:-4]
+
+fh = open(filename1,'r')
+
+fh.readline()
+
+for line in fh.readlines():
+     s = line.strip().split(',')
+     TimeLine = s[3]
+     PostId = s[5]
+     G.add_edge(PostId, TimeLine)
+     G.add_edge(user1, TimeLine)
+    
+fh.close()
+
+filename2 = "feed-100016786692466.csv"
+
+user2 = filename2[5:-4]
+
+fh = open(filename2,'r')
+
+fh.readline()
+
+for line in fh.readlines():
+     s = line.strip().split(',')
+     TimeLine = s[3]
+     PostId = s[5]
+     G.add_edge(PostId, TimeLine)
+     G.add_edge(user2, TimeLine)
+    
+fh.close()
+
+filename3 = "feed-100016788883580.csv"
+
+user3 = filename3[5:-4]
+
+fh = open(filename3,'r')
+
+fh.readline()
+
+for line in fh.readlines():
+     s = line.strip().split(',')
+     TimeLine = s[3]
+     PostId = s[5]
+     G.add_edge(PostId, TimeLine)
+     G.add_edge(user3, TimeLine)
+    
+fh.close()
+
+filename4 = "feed-100016926932367.csv"
+
+user4 = filename4[5:-4]
+
+fh = open(filename4,'r')
+
+fh.readline()
+
+for line in fh.readlines():
+     s = line.strip().split(',')
+     TimeLine = s[3]
+     PostId = s[5]
+     G.add_edge(PostId, TimeLine)
+     G.add_edge(user4, TimeLine)
+    
+fh.close()
+
+
+print len(G), len(G.edges())
+
