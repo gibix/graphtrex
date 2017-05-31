3bca6d8d7b24d4fcaf687ac22113126e6661c500
DoktorFaust <dokfaust@autistici.org>
2017-05-31 11:17:24 +0200

    First analysis, no idea on how to interpret it

--- networkx/Graph.py
+++ networkx/Graph.py
@@ -2,9 +2,14 @@ import networkx as nx
 import random as rnd
 import numpy as np
 import re
+import powerlaw as pwl
 
 from sympy import *
 from lea   import *
+
+#sometimes an error on numpy arrays will show up
+#ignore it
+np.seterr(divide='ignore', invalid='ignore')
 
 G = nx.Graph()
 
@@ -78,4 +83,46 @@ fh.close()
 
 
 print len(G), len(G.edges())
+
+degree = np.array(G.degree().values())
+#for i in degree:
+#    print i
+
+##PARAMETER ESTIMATION###
+#The lib powerlaw allows to estimate the exponent alpha
+# and the minimum value fro the scaling of x_min
+
+fit_function = pwl.Fit(degree)
+fit_function
+fit_function.power_law
+
+fit_function.power_law.alpha
+print fit_function.power_law.alpha
+
+fit_function.power_law.sigma
+print fit_function.power_law.sigma
+
+#The vaue of xmin can be fixed
+#this values should be changed 
+
+fit_function.power_law.xmin
+print fit_function.power_law.xmin
+
+fit_function_fixmin = pwl.Fit(degree, xmin=1)
+fit_function_fixmin.xmin
+print fit_function_fixmin.xmin
+
+fit_function_fixmin.power_law.sigma
+print fit_function_fixmin.power_law.sigma
+
+#We look at the values of teh Kolmogorov-Smirnov
+#distance of the two fits to compare them
+
+fit_function.supported_distributions
+R,p = fit_function.
+fit_function.power_law.D
+print fit_function.power_law.D
+
+fit_function_fixmin.power_law.sigma
+print fit_function_fixmin.power_law.sigma
 
